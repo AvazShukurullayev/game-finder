9b2f5367bb4f3db00145018bd38c6ecbd2048770
Avaz <elonarm21@gmail.com>
2024-04-01 20:10:18 +0500

    Reduxni sozlab olish

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// Setting up bootstrap
 import "bootstrap/dist/css/bootstrap.min.css"
+// Setting up redux
+import {createStore} from "redux";
+// Setting up Provider react ni redux bilan boglash uchun ishlatilinadi
+// react va reduxni bir-biriga ulash uchun kerak, store imizni manage qilish uchun kere boladi.
+import {Provider} from "react-redux";
+// Importing rootReducer, createStore faqat bitta reducer qabul qiladi.
+import rootReducer from "./redux/reducer/index.js";
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
 )
